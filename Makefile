1b048a36e8c3e55083b7af987441a9cac68faa20
Rainer Poisel <rainer@embedded-focus.com>
2025-06-08 14:44:00 +0200

    feat: add FS interaction (read-only for now; rw following soon)

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@
 
 SHELL := /bin/bash
 
+ESP_IDF_DIR= $(shell dirname $$(dirname $$(command -v idf.py)))
+
 all: build-dev
 
 .PHONY: build-dev  # defer all dependency handling to rust/cargo
@@ -25,6 +27,63 @@ run:
 .PHONY: monitor
 monitor:
 	espflash monitor
+
+.PHONY: erase-flash
+erase-flash:
+	esptool.py --chip esp32 erase_flash
+
+.PHONY: build-ui
+build-ui:
+	(cd InterlockingArea-ui && npm run build)
+	echo "Build timestamp: $$(date)" > InterlockingArea-ui/dist/build.ts
+
+create-webapp-fs: build-ui
+	$(ESP_IDF_DIR)/components/fatfs/fatfsgen.py \
+		--sector_size 4096 \
+		--partition_size $$((1024 * 1024)) \
+		--output_file webapp_fs.bin \
+		InterlockingArea-ui/dist
+
+.PHONY: create-user-fs
+create-user-fs:
+	# 500kB - size of WL (wear-leveling) header
+	$(ESP_IDF_DIR)/components/fatfs/fatfsgen.py \
+		--sector_size 4096 \
+		--partition_size $$((512 * 1024 - 4096)) \
+		--output_file user_fs.bin \
+		user_data
+
+.PHONY: parse-fat-fs
+parse-fat-fs:
+	$(ESP_IDF_DIR)/components/fatfs/fatfsparse.py \
+		webapp_fs.bin
+
+wl-header.bin:
+	# the wear-leveling (WL) header needs to be present before the actual FAT FS
+	# as we are not using any wear-leveling, it consists of only 0xFF values
+	dd if=/dev/zero bs=1 count=4096 | tr '\000' '\377' > $@
+
+flash-user-fs: create-user-fs wl-header.bin
+	# https://www.reddit.com/r/esp32/comments/plvaq3/comment/hcdmsir/
+	esptool.py --chip esp32 -p /dev/ttyUSB0 -b 460800 \
+		--before=default_reset --after=hard_reset \
+		write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB \
+		0x310000 wl-header.bin \
+		0x311000 user_fs.bin
+
+flash-webapp-fs: create-webapp-fs
+	# https://www.reddit.com/r/esp32/comments/plvaq3/comment/hcdmsir/
+	esptool.py --chip esp32 -p /dev/ttyUSB0 -b 460800 \
+		--before=default_reset --after=hard_reset \
+		write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB \
+		0x210000 webapp_fs.bin
+
+.PHONY: flash-all
+flash-all:
+	$(MAKE) erase-flash
+	$(MAKE) flash-webapp-fs
+	$(MAKE) flash-user-fs
+	$(MAKE) run
 
 # usbip
 # - https://www.unifix.org/2023/11/28/usbip-on-debian-12-usb-device-sharing-over-ip-network/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use core::ffi::c_char;
+use core::ffi::c_int;
+use core::slice;
+use core::str;
 use embedded_svc::http::Method::Get;
 use embedded_svc::io::Write;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration as WifiConfiguration};
@@ -6,16 +10,21 @@ use esp_idf_hal::{peripheral::Peripheral, prelude::Peripherals};
 use esp_idf_svc::io::EspIOError;
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    http::server::EspHttpServer,
+    http::server::{Configuration as HttpConfiguration, EspHttpServer},
     nvs::{EspDefaultNvsPartition, EspNvsPartition, NvsDefault},
     ping::Configuration as PingConfiguration,
     ping::EspPing,
     timer::{EspTaskTimerService, EspTimerService, Task},
     wifi::{AsyncWifi, EspWifi},
 };
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_sys::{
+    self as _, esp_vfs_fat_mount_config_t, esp_vfs_fat_spiflash_mount_ro,
+    esp_vfs_fat_spiflash_mount_rw_wl, fclose, fgets, fopen, wl_handle_t, CONFIG_WL_SECTOR_SIZE,
+    ESP_OK, WL_INVALID_HANDLE,
+}; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use heapless::String;
 use log::*;
+use std::ffi::CString;
 
 macro_rules! env_with_default {
     ($var:literal, $default:expr) => {
@@ -28,6 +37,21 @@ macro_rules! env_with_default {
 
 const SSID: &str = env_with_default!("RUST_ESP32_STD_DEMO_WIFI_SSID", "MyDefaultSSID");
 const PASS: &str = env_with_default!("RUST_ESP32_STD_DEMO_WIFI_PASS", "MyDefaultPass");
+
+unsafe fn c_char_to_u8_slice(ptr: *const c_char) -> &'static [u8] {
+    if ptr.is_null() {
+        return &[];
+    }
+
+    // Calculate length by scanning for the null terminator
+    let mut len = 0;
+    while *ptr.add(len) != 0 {
+        len += 1;
+    }
+
+    // Reinterpret the pointer as *const u8 and create a slice
+    slice::from_raw_parts(ptr, len)
+}
 
 pub fn wifi(
     modem: impl Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
@@ -65,13 +89,13 @@ async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<
     wifi.set_configuration(&wifi_configuration)?;
 
     wifi.start().await?;
-    info!("Wifi started");
+    info!("Wifi started.");
 
     wifi.connect().await?;
-    info!("Wifi connected");
+    info!("Wifi connected.");
 
     wifi.wait_netif_up().await?;
-    info!("Wifi netif up");
+    info!("Wifi netif up.");
 
     Ok(())
 }
@@ -98,20 +122,72 @@ fn main() {
     )
     .unwrap();
 
-    let mut server = EspHttpServer::new(&Default::default()).unwrap();
+    let base_path = CString::new("/webapp").unwrap(); // Make sure it has no interior nulls
+    let partition_label = CString::new("webapp").unwrap();
+    let config = esp_vfs_fat_mount_config_t {
+        format_if_mount_failed: false,
+        max_files: 4,
+        allocation_unit_size: CONFIG_WL_SECTOR_SIZE as usize,
+        disk_status_check_enable: false,
+        use_one_fat: false,
+    };
+    let mut wl_handle: wl_handle_t = WL_INVALID_HANDLE;
+    let result = unsafe {
+        esp_vfs_fat_spiflash_mount_ro(
+            base_path.as_ptr(),
+            partition_label.as_ptr(),
+            &config as *const _,
+            // &mut wl_handle,
+        )
+    };
+
+    match result {
+        ESP_OK => {
+            info!("Mount FAT partition successful.");
+        }
+        _ => {
+            error!(
+                "An error occurred mounting the FAT partition. Result code: {}",
+                result
+            );
+        }
+    }
+
+    let server_config = HttpConfiguration {
+        uri_match_wildcard: true,
+        ..Default::default()
+    };
+    let mut server = EspHttpServer::new(&server_config).unwrap();
 
     server
-        .fn_handler("/hello", Get, move |req| {
+        .fn_handler("/webapp/*", Get, move |req| {
+            info!("Got URI: {}", req.uri());
+            let fh = unsafe {
+                fopen(
+                    CString::new("/webapp/build.ts").unwrap().as_ptr(),
+                    CString::new("r").unwrap().as_ptr(),
+                )
+            };
+            if fh.is_null() {
+                let mut resp =
+                    req.into_response(404, Some("Not found."), &[("Content-Type", "text/html")])?;
+                resp.write_all(b"No suitable file could be found.")?;
+                return Ok::<(), EspIOError>(());
+            }
+
             let mut resp = req.into_response(200, Some("OK"), &[("Content-Type", "text/html")])?;
 
-            // Send the HTML body
-            resp.write_all(
-                b"<!DOCTYPE html>
-            <html>
-            <head><title>Hello</title></head>
-            <body><h1>Hello from ESP32!</h1></body>
-            </html>",
-            )?;
+            let mut buf = [0u8; 128];
+            loop {
+                let line =
+                    unsafe { fgets(buf.as_mut_ptr() as *mut c_char, buf.len() as c_int, fh) };
+                if line.is_null() {
+                    break;
+                }
+                resp.write_all(unsafe { c_char_to_u8_slice(line) })?;
+            }
+            unsafe { fclose(fh) };
+
             Ok::<(), EspIOError>(())
         })
         .unwrap();
